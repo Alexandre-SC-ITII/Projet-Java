1d0c9451483db04113c52aac6bad0ead9394d77c
Alexandre-SC-ITII <a.scanzalupi@gmail.com>
2023-04-21 11:45:05 +0200

    Update Heros.java

--- src/matrice/Heros.java
+++ src/matrice/Heros.java
@@ -1,5 +1,17 @@
 package matrice;
 
 public class Heros {
+	private String nom;
+	private Ressource[] ble;
+	private Ressource[] bois;
+	private Ressource pierre;
+	private ObjetManufacture Feu;
+	private ObjetManufacture Farine;
+	private int poidsEmporte = 0;
+	private int nbPartie;
+	private int[] caseCourante;
 
+	public Heros(String nom) {
+		
+	}
 }
